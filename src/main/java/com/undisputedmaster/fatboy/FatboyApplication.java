823da8c7fa05d828b8ceb813bb1fbbbb493ec33d
scorpionking94 <40591187+scorpionking94@users.noreply.github.com>
2019-06-12 11:03:52 +0530

    Update FatboyApplication.java

--- src/main/java/com/undisputedmaster/fatboy/FatboyApplication.java
+++ src/main/java/com/undisputedmaster/fatboy/FatboyApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FatboyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FatboyApplication.class, args);
 	}
